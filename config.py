f5596d39ca498ccc1d87e424fe20aae5ee95cfe7
MaxxRider2 <66148227+MaxxRider2@users.noreply.github.com>
2020-08-05 20:26:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ apiKey = "halyoa"
 apiParams = f"&sort=0&category=0&page=0&adult=0&key={apiKey}&concurrent=0&provider_ids[]=32"
 botToken = "1074120539:AAEOwqBbMBFoMzwaOJXqwvnVu1uXrVn-yJs"
 supportChatUrl = "https://t.me/MaxxBotChat"
-appUrl = 'https://play.google.com/store/apps/details?id=com.icodelife.itorrentsearch'
+appUrl = 'https://t.me/MaxxBots'
