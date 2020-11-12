52eeabd8b7b85bd04d5a00d9cc08855c611945c8
KGK06 <72903548+KGK06@users.noreply.github.com>
2020-11-12 06:51:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ apiKey = "halyoa"
 # API KEY FOR THE API IS PUBLIC
 # DON'T ABUSE THE API
 apiParams = f"&sort=0&category=0&page=0&adult=0&key={apiKey}&concurrent=0&provider_ids[]=32"
-botToken = "1074120539:AAEOwqBbMBFoMzwaOJXqwvnVu1uXrVn-yJs"
-supportChatUrl = "https://t.me/MaxxBotChat"
-appUrl = 'https://t.me/MaxxBots'
+botToken = "1449538840:AAGdV1LmECtmkz_oN0x1f-K-9TlpS1EQK8s"
+supportChatUrl = "https://t.me/MeGBotsChat"
+appUrl = 'https://t.me/MeGBots'
